c28eccf7bcf95a66451c6ac90cc34d5a793a4944
NT <86037419+inverter3p@users.noreply.github.com>
2024-10-19 17:57:15 +0700

    Update spwm_plot.py

--- spwm_plot.py
+++ spwm_plot.py
@@ -35,4 +35,61 @@ st.image(image)
 # Modulation parameters
 st.info('** Modulation parameters**')
 col1, col3 = st.columns([1, 1])
-with
+with col1:
+    st.write(f"$$M_a = {x}$$")
+    st.write(f"$$F_c = {fcarrier}$$")
+    st.sidebar.info('** 2. Set DC link voltage (VDC)**')
+    Vdc = st.sidebar.slider('Vdc', 100, 600, 100)
+    st.write(f"$$V_{{DC}} = {Vdc} \: V$$")
+
+# Plotting modulation signals
+plt.style.use('ggplot')
+sample_rate = 4000
+duration = 1
+time = np.arange(0, duration, 1 / sample_rate)
+pii = np.pi
+ma = x
+
+fc = fcarrier * 1  # Carrier frequency
+tri = signal.sawtooth(2 * pii * fc * time, 0.5)
+tri_FB = tri * 1
+
+# Generate sine waves for different phases
+sine_A = ma * np.sin(2 * pii * time)
+sine_B = ma * np.sin(2 * pii * time + 2 * pii / 3)
+sine_C = ma * np.sin(2 * pii * time - 2 * pii / 3)
+
+# Generate plot based on selected PWM scheme
+plotpwm = plt.figure(figsize=[7, 3])
+plt.plot(time, tri_FB, 'gray')
+plt.plot(time, sine_A, 'red')
+plt.axis([0, 1, -1.5, 1.5])
+plt.xticks([])
+plt.yticks([-0.5, 0, -1, 0.5, 1])
+plt.text(0.25, 1.05, 'a', color='r')
+
+if pwm == '1P Unipolar SPWM':
+    plt.plot(time, -sine_A, 'blue')
+    plt.text(0.92, 1.05, 'b', color='b')
+if pwm == '3P SPWM':
+    plt.plot(time, sine_B, 'blue')
+    plt.plot(time, sine_C, 'black')
+    plt.text(0.92, 1.05, 'b', color='b')
+    plt.text(0.57, 1.05, 'c', color='k')
+
+# Display the plot using the latest Streamlit function
+st.pyplot(plotpwm)
+
+# Phase-leg voltage calculation and plotting
+pwm_A = (sine_A >= tri_FB) * 1
+VA = pwm_A * Vdc
+Vsin_A = (sine_A + 1) * 0.5 * Vdc
+phase_A = plt.figure(figsize=[7, 3])
+plt.plot(time, VA, 'gray')
+plt.plot(time, Vsin_A, 'red')
+plt.text(0.3, max(Vsin_A) + 2, f'{max(Vsin_A):.2f} V')
+plt.axis([0, 1, -0.25 * Vdc, 1.25 * Vdc])
+plt.xticks([])
+plt.yticks([0, 0.5 * Vdc, Vdc])
+
+st.pyplot(phase_A)
